644345e78206de450684af378122f930d0016eef
majestymewtwo <majestymewtwo@gmail.com>
2023-01-25 12:45:07 +0530

    made error page into a loading page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,11 +3,11 @@ import "./Main.css";
 function ErrorPage(){
     return(
         <div className="error-page text-center bg-dark text-white">
-            <h1 className="heading text-warning">Error 404</h1>
-            <h5 className="desc">Whoops! There seems to be some error</h5>
+            <h1 className="heading text-warning">0___0</h1>
+            <h5 className="desc">Please wait, page is loading</h5>
             <br />
             <a href='/' className='text-warning'>
-              Try logging in
+              Go back to login page
             </a>
         </div>
     )
